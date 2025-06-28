18fafb2c3839d6bc3c0ce70f33f24f9275e2759f
Ashish Khawas <khawasashish950@gmail.com>
2025-06-28 08:30:52 +0530

    Add files via upload

---
+++ array_extreme_points_.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int arr[9]={1,2,3,4,5,6,7,8,9};
+    
+    int size=9;
+    
+    int start=0;
+    int end=size-1;
+    
+    while(start<=end){
+        if(start==end)
+        {
+            cout<<arr[start]<<endl;
+        }
+        
+        else{
+            cout<<arr[start];
+            cout<<arr[end];
+            
+        }
+        start++;
+            end--;
+    }
+    
+    return 0;
+    
+}
